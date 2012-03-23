68d6225d2f195a3aa5aca2429d1e559774839885
corsen <bcchaiklin@gmail.com>
2012-03-22 21:57:11 -0400

    Formatting Simple Search and adding a force option
    Adding some formatting to the simple search container to make
    things look a little nicer.
    
    Also adding a force option to Sno to force copying over assets.
    Eventually we will want to auto detect if they have changed and
    auto force a copy when needed.  For now this works.

--- lib/sno.rb
+++ lib/sno.rb
@@ -8,16 +8,16 @@ module Sno
 		Dir.mkdir(output_dir) unless Dir.exists? output_dir
 		Dir.mkdir(assets_root) unless Dir.exists? assets_root
 
-		copy_if_needed "#{SNO_ROOT}/assets/base/global.css", "#{assets_root}/global.css"
+		copy_if_needed "#{SNO_ROOT}/assets/base/global.css", "#{assets_root}/global.css", options
 		options[:css_file] = "global"
 		options[:assets_root] = assets_root
 
-		copy_if_needed "#{SNO_ROOT}/assets/lib/jquery-1.7.2.js", "#{assets_root}/jquery.js"
-		copy_if_needed "#{SNO_ROOT}/assets/base/simple_search.js", "#{assets_root}/simple_search.js"
-		copy_if_needed "#{SNO_ROOT}/assets/lib/jquery-ui-1.8.18.custom.min.js", "#{assets_root}/jquery-ui.js"
-		copy_if_needed "#{SNO_ROOT}/assets/lib/jquery-ui-1.8.18.custom.css", "#{assets_root}/jquery-ui.css"
-		copy_if_needed "#{SNO_ROOT}/assets/lib/images", "#{assets_root}/images"
-		copy_if_needed "#{SNO_ROOT}/assets/base/.snoignore", "#{input_dir}/.snoignore"
+		copy_if_needed "#{SNO_ROOT}/assets/lib/jquery-1.7.2.js", "#{assets_root}/jquery.js", options
+		copy_if_needed "#{SNO_ROOT}/assets/base/simple_search.js", "#{assets_root}/simple_search.js", options
+		copy_if_needed "#{SNO_ROOT}/assets/lib/jquery-ui-1.8.18.custom.min.js", "#{assets_root}/jquery-ui.js", options
+		copy_if_needed "#{SNO_ROOT}/assets/lib/jquery-ui-1.8.18.custom.css", "#{assets_root}/jquery-ui.css", options
+		copy_if_needed "#{SNO_ROOT}/assets/lib/images", "#{assets_root}/images", options
+		copy_if_needed "#{SNO_ROOT}/assets/base/.snoignore", "#{input_dir}/.snoignore", options
 
 		File.open("#{input_dir}/.snoignore").each do |line|
 			Extractor.add_ignore("#{input_dir}/#{line.chomp}")
@@ -26,9 +26,8 @@ module Sno
 		[Extractor.extractor_for(input_dir).new(input_dir, output_dir, options), assets_root]
 	end
 
-	def copy_if_needed(source, dest, dir = false)
-		unless File.exists? dest
-			puts "copying..."
+	def copy_if_needed(source, dest, options, dir = false)
+		if !File.exists?(dest) || options[:force]
 			if dir
 				FileUtils.copy source, dest
 			else
