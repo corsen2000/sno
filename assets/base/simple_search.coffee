69869d324cf1a453b2aba2c2cc6260a4f60954bd
Bryan Chaiklin <bcchaiklin@gmail.com>
2012-03-24 08:57:19 -0400

    Converting simple_search.js to CoffeeScript
    Now using CoffeeScript for simple_search.  As with
    our Sass stylesheet, we continue to check in both the
    source and compiled version of the file.  We don't want
    to have these things as dependencies to use Sno.  Especially
    CoffeeScript which requires more than just Ruby.

---
+++ assets/base/simple_search.coffee
@@ -0,0 +1,13 @@
+jQuery ->
+    $("#search").autocomplete {
+        source: searchIndex
+        autoFocus: true
+        delay: 0
+        focus: (event, ui) ->
+            if /^(key)/.test(event.originalEvent.originalEvent.type)
+                $("#search").val(ui.item.display)
+            false
+        select: (event, ui) ->
+            window.location.href = "file://" + ui.item.value
+            false
+        }
--- assets/base/simple_search.js
+++ assets/base/simple_search.js
@@ -1,17 +1,21 @@
-$(function() {
-	$( "#search" ).autocomplete({
-		source: searchIndex,
-		autoFocus: true,
-		delay: 0,
-		focus: function(event, ui) {				
-			if ( /^(key)/.test(event.originalEvent.originalEvent.type) ) {
-				$("#search").val(ui.item.display);
-			}				
-			return false;
-		},
-		select: function(event, ui) {
-			window.location.href = "file://" + ui.item.value;
-			return false;
-		}
-	});
-});
+(function() {
+
+  jQuery(function() {
+    return $("#search").autocomplete({
+      source: searchIndex,
+      autoFocus: true,
+      delay: 0,
+      focus: function(event, ui) {
+        if (/^(key)/.test(event.originalEvent.originalEvent.type)) {
+          $("#search").val(ui.item.display);
+        }
+        return false;
+      },
+      select: function(event, ui) {
+        window.location.href = "file://" + ui.item.value;
+        return false;
+      }
+    });
+  });
+
+}).call(this);
