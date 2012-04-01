c22e51b5be63dc7f604bba7b7bb8c8c07d17b2e5
Bryan Chaiklin <bcchaiklin@gmail.com>
2012-04-01 16:06:18 -0400

    Using spaces instead of tabs everywhere.
    Converting all tabs to spaces.  This is the convetion in Ruby
    and a good first step to the refactor.

--- lib/sno.rb
+++ lib/sno.rb
@@ -1,38 +1,38 @@
 require "sno/extractor_base"
 
 module Sno
-	def sno_init(input_dir, output_dir, options)
-		input_dir = File.expand_path(input_dir)
-		assets_root = "#{output_dir}/.sno"
+  def sno_init(input_dir, output_dir, options)
+    input_dir = File.expand_path(input_dir)
+    assets_root = "#{output_dir}/.sno"
 
-		Dir.mkdir(output_dir) unless Dir.exists? output_dir
-		Dir.mkdir(assets_root) unless Dir.exists? assets_root
+    Dir.mkdir(output_dir) unless Dir.exists? output_dir
+    Dir.mkdir(assets_root) unless Dir.exists? assets_root
 
-		copy_if_needed "#{SNO_ROOT}/assets/base/global.css", "#{assets_root}/global.css", options
-		options[:css_file] = "global"
-		options[:assets_root] = assets_root
+    copy_if_needed "#{SNO_ROOT}/assets/base/global.css", "#{assets_root}/global.css", options
+    options[:css_file] = "global"
+    options[:assets_root] = assets_root
 
-		copy_if_needed "#{SNO_ROOT}/assets/lib/jquery-1.7.2.js", "#{assets_root}/jquery.js", options
-		copy_if_needed "#{SNO_ROOT}/assets/base/simple_search.js", "#{assets_root}/simple_search.js", options
-		copy_if_needed "#{SNO_ROOT}/assets/lib/jquery-ui-1.8.18.custom.min.js", "#{assets_root}/jquery-ui.js", options
-		copy_if_needed "#{SNO_ROOT}/assets/lib/jquery-ui-1.8.18.custom.css", "#{assets_root}/jquery-ui.css", options
-		copy_if_needed "#{SNO_ROOT}/assets/lib/images", "#{assets_root}/images", options
-		copy_if_needed "#{SNO_ROOT}/assets/base/.snoignore", "#{input_dir}/.snoignore", options
+    copy_if_needed "#{SNO_ROOT}/assets/lib/jquery-1.7.2.js", "#{assets_root}/jquery.js", options
+    copy_if_needed "#{SNO_ROOT}/assets/base/simple_search.js", "#{assets_root}/simple_search.js", options
+    copy_if_needed "#{SNO_ROOT}/assets/lib/jquery-ui-1.8.18.custom.min.js", "#{assets_root}/jquery-ui.js", options
+    copy_if_needed "#{SNO_ROOT}/assets/lib/jquery-ui-1.8.18.custom.css", "#{assets_root}/jquery-ui.css", options
+    copy_if_needed "#{SNO_ROOT}/assets/lib/images", "#{assets_root}/images", options
+    copy_if_needed "#{SNO_ROOT}/assets/base/.snoignore", "#{input_dir}/.snoignore", options
 
-		File.open("#{input_dir}/.snoignore").each do |line|
-			Extractor.add_ignore("#{input_dir}/#{line.chomp}")
-		end
+    File.open("#{input_dir}/.snoignore").each do |line|
+      Extractor.add_ignore("#{input_dir}/#{line.chomp}")
+    end
 
-		[Extractor.extractor_for(input_dir).new(input_dir, output_dir, options), assets_root]
-	end
+    [Extractor.extractor_for(input_dir).new(input_dir, output_dir, options), assets_root]
+  end
 
-	def copy_if_needed(source, dest, options, dir = false)
-		if !File.exists?(dest) || options[:force]
-			if dir
-				FileUtils.copy source, dest
-			else
-				FileUtils.copy_entry source, dest
-			end
-		end
-	end
+  def copy_if_needed(source, dest, options, dir = false)
+    if !File.exists?(dest) || options[:force]
+      if dir
+        FileUtils.copy source, dest
+      else
+        FileUtils.copy_entry source, dest
+      end
+    end
+  end
 end
--- lib/sno/extractor_base.rb
+++ lib/sno/extractor_base.rb
@@ -8,252 +8,252 @@ require "redcloth"
 require "redcarpet"
 
 module Sno
-	class Extractor
-		@@file_matchers = []
-		@@directory_matchers = []
-		@@ignore_patterns = []
-		attr_accessor :file_path
-		attr_accessor :output_dir
-		attr_accessor :options
+  class Extractor
+    @@file_matchers = []
+    @@directory_matchers = []
+    @@ignore_patterns = []
+    attr_accessor :file_path
+    attr_accessor :output_dir
+    attr_accessor :options
 
-		def initialize(file_path, output_dir, options = {})
-			@file_path = file_path
-			@output_dir = output_dir
-			@options = options
-		end
+    def initialize(file_path, output_dir, options = {})
+      @file_path = file_path
+      @output_dir = output_dir
+      @options = options
+    end
 
-		def directory?
-			false
-		end
+    def directory?
+      false
+    end
 
-		def save
-			FileUtils.copy file_path, output_path
-		end
+    def save
+      FileUtils.copy file_path, output_path
+    end
 
-		private
-		def output_path
-			"#{output_dir}/#{output_name}"
-		end
+    private
+    def output_path
+      "#{output_dir}/#{output_name}"
+    end
 
-		def output_name
-			File.basename(file_path, ".*") + output_extension
-		end
+    def output_name
+      File.basename(file_path, ".*") + output_extension
+    end
 
-		def output_extension
-			File.extname file_path
-		end
+    def output_extension
+      File.extname file_path
+    end
 
-		def self.add_matcher(matcher, directory = false)
-			if directory
-				@@directory_matchers << matcher
-			else
-				@@file_matchers << matcher
-			end		
-		end
+    def self.add_matcher(matcher, directory = false)
+      if directory
+        @@directory_matchers << matcher
+      else
+        @@file_matchers << matcher
+      end   
+    end
 
-		def self.add_ignore(regex)
-			@@ignore_patterns << regex
-		end
+    def self.add_ignore(regex)
+      @@ignore_patterns << regex
+    end
 
-		def self.extractor_for(file_path)
-			unless @@ignore_patterns.any? { |pattern| !file_path.match(pattern).nil? }
-				matchers = File.directory?(file_path) ? @@directory_matchers : @@file_matchers 
-				filename = File.basename file_path
-				matcher = matchers.detect do |matcher|
-					matcher[:expressions].any? do |expression|
-						expression.match(filename)
-					end
-				end
-				matcher[:class] unless matcher.nil?
-			end
-		end
-	end
+    def self.extractor_for(file_path)
+      unless @@ignore_patterns.any? { |pattern| !file_path.match(pattern).nil? }
+        matchers = File.directory?(file_path) ? @@directory_matchers : @@file_matchers 
+        filename = File.basename file_path
+        matcher = matchers.detect do |matcher|
+          matcher[:expressions].any? do |expression|
+            expression.match(filename)
+          end
+        end
+        matcher[:class] unless matcher.nil?
+      end
+    end
+  end
 
-	class Linker < Extractor
-		def link_type
-			:raw
-		end
-		def link
-			"#{File.basename output_dir}/#{output_name}"
-		end
-	end
-	Extractor.add_matcher({:class => Linker, :expressions => [/.*\.pdf/]})
+  class Linker < Extractor
+    def link_type
+      :raw
+    end
+    def link
+      "#{File.basename output_dir}/#{output_name}"
+    end
+  end
+  Extractor.add_matcher({:class => Linker, :expressions => [/.*\.pdf/]})
 
-	class Page < Linker
-		@@pages = []
-		include ActiveSupport::Inflector
-		attr_accessor :content
-		attr_accessor :name
-		attr_accessor :root_href
-		attr_accessor :bread_crumbs
+  class Page < Linker
+    @@pages = []
+    include ActiveSupport::Inflector
+    attr_accessor :content
+    attr_accessor :name
+    attr_accessor :root_href
+    attr_accessor :bread_crumbs
 
-		def initialize(file_path, output_dir, options = {})
-			super file_path, output_dir, options
-			@name = File.basename file_path, ".*"		
-			options[:root_path] ||= output_path
-			@root_href = Pathname.new(options[:root_path]).relative_path_from Pathname.new output_dir
-			@bread_crumbs = options[:bread_crumbs] || []
-			bread_crumbs << "#{output_dir}/#{output_name}"
-			@@pages << {
-				:label => "#{titleize(@name)} (#{titleize(File.basename output_dir)})",
-				:value => File.expand_path("#{output_dir}/#{output_name}"), 
-				:display => "#{output_dir}/#{@name}".sub(OUTPUT_ROOT, "").sub(/.*?\//, "")
-			}
-		end
+    def initialize(file_path, output_dir, options = {})
+      super file_path, output_dir, options
+      @name = File.basename file_path, ".*"   
+      options[:root_path] ||= output_path
+      @root_href = Pathname.new(options[:root_path]).relative_path_from Pathname.new output_dir
+      @bread_crumbs = options[:bread_crumbs] || []
+      bread_crumbs << "#{output_dir}/#{output_name}"
+      @@pages << {
+        :label => "#{titleize(@name)} (#{titleize(File.basename output_dir)})",
+        :value => File.expand_path("#{output_dir}/#{output_name}"), 
+        :display => "#{output_dir}/#{@name}".sub(OUTPUT_ROOT, "").sub(/.*?\//, "")
+      }
+    end
 
-		def self.pages
-			@@pages
-		end
+    def self.pages
+      @@pages
+    end
 
-		def javascripts
-			%w(jquery jquery-ui simple_search search_index).map do |js|
-				Pathname.new("#{options[:assets_root]}/#{js}.js").relative_path_from Pathname.new output_dir
-			end
-		end
+    def javascripts
+      %w(jquery jquery-ui simple_search search_index).map do |js|
+        Pathname.new("#{options[:assets_root]}/#{js}.js").relative_path_from Pathname.new output_dir
+      end
+    end
 
-		def search_json_path
-			Pathname.new("#{options[:assets_root]}/search_index.json").relative_path_from Pathname.new output_dir
-		end
+    def search_json_path
+      Pathname.new("#{options[:assets_root]}/search_index.json").relative_path_from Pathname.new output_dir
+    end
 
-		def stylesheets
-			["jquery-ui", options[:css_file]].map do |css|
-				Pathname.new("#{options[:assets_root]}/#{css}.css").relative_path_from Pathname.new output_dir
-			end
-		end
+    def stylesheets
+      ["jquery-ui", options[:css_file]].map do |css|
+        Pathname.new("#{options[:assets_root]}/#{css}.css").relative_path_from Pathname.new output_dir
+      end
+    end
 
-		def bread_crumbs_rel
-			@bread_crumbs.map do |url|
-				Pathname.new(url).relative_path_from Pathname.new output_dir
-			end
-		end
+    def bread_crumbs_rel
+      @bread_crumbs.map do |url|
+        Pathname.new(url).relative_path_from Pathname.new output_dir
+      end
+    end
 
-		def title
-			options[:project_name] ||= titleize(name)
-		end
+    def title
+      options[:project_name] ||= titleize(name)
+    end
 
-		def header
-			name
-		end
+    def header
+      name
+    end
 
-		def content
-			@content ||= extract_content
-		end
+    def content
+      @content ||= extract_content
+    end
 
-		def output_extension
-			".html"
-		end
+    def output_extension
+      ".html"
+    end
 
-		def to_html
-			page_engine = Haml::Engine.new(File.read("#{TEMPLATE_ROOT}/layout.haml"), :ugly => true)
-			page_engine.render(self)
-		end
+    def to_html
+      page_engine = Haml::Engine.new(File.read("#{TEMPLATE_ROOT}/layout.haml"), :ugly => true)
+      page_engine.render(self)
+    end
 
-		def link_type
-			:internal
-		end
+    def link_type
+      :internal
+    end
 
-		def save
-			File.open(output_path, "w+") do |file|
-				file.puts self.to_html
-			end
-		end
-	end
+    def save
+      File.open(output_path, "w+") do |file|
+        file.puts self.to_html
+      end
+    end
+  end
 
-	class IndexPage < Page
-		attr_reader :children
+  class IndexPage < Page
+    attr_reader :children
 
-		def extract_content()
-			@children = []
-			Dir.glob("#{file_path}/*").each do |file|
-				extractor = create_extractor(file)
-				if extractor && !extractor.link.nil?
-					children << extractor
-				end
-			end
-			unless children.empty?
-				index_engine = Haml::Engine.new(File.read("#{TEMPLATE_ROOT}/index.haml"))
-				index_engine.render(self)
-			end
-		end
+    def extract_content()
+      @children = []
+      Dir.glob("#{file_path}/*").each do |file|
+        extractor = create_extractor(file)
+        if extractor && !extractor.link.nil?
+          children << extractor
+        end
+      end
+      unless children.empty?
+        index_engine = Haml::Engine.new(File.read("#{TEMPLATE_ROOT}/index.haml"))
+        index_engine.render(self)
+      end
+    end
 
-		def directory?
-			true		
-		end
+    def directory?
+      true    
+    end
 
-		def directories
-			children.reject { |item| !item.directory? }	
-		end
+    def directories
+      children.reject { |item| !item.directory? } 
+    end
 
-		def files
-			children.reject { |item| item.directory? }
-		end
+    def files
+      children.reject { |item| item.directory? }
+    end
 
-		def link
-			super unless content.nil?
-		end
+    def link
+      super unless content.nil?
+    end
 
-		def save
-			super
-			children.each do |page|
-				Dir.mkdir(page.output_dir) unless Dir.exists? page.output_dir
-				page.save
-			end
-		end
+    def save
+      super
+      children.each do |page|
+        Dir.mkdir(page.output_dir) unless Dir.exists? page.output_dir
+        page.save
+      end
+    end
 
-		private
-		def create_extractor(file)		
-			extractor = Extractor.extractor_for(file)
-			options = self.options.clone
-			options[:bread_crumbs] = [] + bread_crumbs
-			extractor.new(file, "#{output_dir}/#{File.basename file_path}", options) if extractor
-		end
-	end
-	Extractor.add_matcher({:class => IndexPage, :expressions => [/.*/]}, true)
+    private
+    def create_extractor(file)    
+      extractor = Extractor.extractor_for(file)
+      options = self.options.clone
+      options[:bread_crumbs] = [] + bread_crumbs
+      extractor.new(file, "#{output_dir}/#{File.basename file_path}", options) if extractor
+    end
+  end
+  Extractor.add_matcher({:class => IndexPage, :expressions => [/.*/]}, true)
 
-	class TextPage < Page
-		def extract_content
-			CGI::escapeHTML File.read(file_path)
-		end
-	end
-	Extractor.add_matcher({:class => TextPage, :expressions => [/.*\.txt/]})
+  class TextPage < Page
+    def extract_content
+      CGI::escapeHTML File.read(file_path)
+    end
+  end
+  Extractor.add_matcher({:class => TextPage, :expressions => [/.*\.txt/]})
 
-	class HtmlPage < Page
-		def extract_content
-			File.read file_path
-		end
-	end
-	Extractor.add_matcher({:class => HtmlPage, :expressions => [/.*\.html/]})
+  class HtmlPage < Page
+    def extract_content
+      File.read file_path
+    end
+  end
+  Extractor.add_matcher({:class => HtmlPage, :expressions => [/.*\.html/]})
 
-	class HamlPage < Page
-		def extract_content
-			Haml::Engine.new(File.read(file_path)).render
-		end
-	end
-	Extractor.add_matcher({:class => HamlPage, :expressions => [/.*\.haml/]})
+  class HamlPage < Page
+    def extract_content
+      Haml::Engine.new(File.read(file_path)).render
+    end
+  end
+  Extractor.add_matcher({:class => HamlPage, :expressions => [/.*\.haml/]})
 
-	class ImagePage < Page
-		def extract_content
-			base64 = Base64.encode64(File.read(file_path))		
-			"<img src=\"data:image/#{File.extname file_path};base64,#{base64}\" />"
-		end
-	end
-	Extractor.add_matcher({:class => ImagePage, :expressions => [/.*\.jpg/]})
+  class ImagePage < Page
+    def extract_content
+      base64 = Base64.encode64(File.read(file_path))    
+      "<img src=\"data:image/#{File.extname file_path};base64,#{base64}\" />"
+    end
+  end
+  Extractor.add_matcher({:class => ImagePage, :expressions => [/.*\.jpg/]})
 
-	class TextilePage < Page
-		def extract_content
-			RedCloth.new(File.read(file_path)).to_html
-		end
-	end
-	Extractor.add_matcher({:class => TextilePage, :expressions => [/.*\.textile/]})
+  class TextilePage < Page
+    def extract_content
+      RedCloth.new(File.read(file_path)).to_html
+    end
+  end
+  Extractor.add_matcher({:class => TextilePage, :expressions => [/.*\.textile/]})
 
-	class MarkDownPage < Page
-		parse_options = { :autolink => true, :space_after_headers => true, :fenced_code_blocks => true, :no_intra_emphasis => true }
-		render_options = { :hard_wrap => true }
-		@@markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML.new(render_options), parse_options) 
+  class MarkDownPage < Page
+    parse_options = { :autolink => true, :space_after_headers => true, :fenced_code_blocks => true, :no_intra_emphasis => true }
+    render_options = { :hard_wrap => true }
+    @@markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML.new(render_options), parse_options) 
 
-		def extract_content
-			@@markdown.render(File.read(file_path))
-		end
-	end
-	Extractor.add_matcher({:class => MarkDownPage, :expressions => [/.*\.md/]})
+    def extract_content
+      @@markdown.render(File.read(file_path))
+    end
+  end
+  Extractor.add_matcher({:class => MarkDownPage, :expressions => [/.*\.md/]})
 end
